6d08b3c058182b3ddd195d83ab281f09aa78cf42
Adyuta Indra Adyatma <44364705+adyuttta@users.noreply.github.com>
2021-11-20 09:56:35 +0700

    Add files via upload

---
+++ C_to_F_exercise2.py
@@ -0,0 +1,9 @@
+#konversi celcius ke fahrenheit
+celcius = float(input("Masukkan suhu celcius: "))
+fahrenheit = (celcius * 9.0/5.0) + 32
+print(fahrenheit)
+
+#konversi fahrenheit ke celcius
+fahrenheit = float(input("Masukkan suhu fahrenheit: "))
+celcius = (fahrenheit -32)*(5.0/9.0)
+print(celcius)
